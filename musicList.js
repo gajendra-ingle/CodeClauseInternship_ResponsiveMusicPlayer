22e37d44b2559942a87a7a8156a1a1bebce9f10c
gajendra-ingle <gajendraingle01@gmail.com>
2024-01-04 22:15:27 +0530

    code is added

---
+++ musicList.js
@@ -0,0 +1,39 @@
+
+let allMusic = [
+    {
+      name: "Saajan Ve",
+      artist: "Darshan Raval",
+      img: "music-1",
+      src: "music-1"
+    },
+    {
+      name: "Maahi",
+      artist: "Madhur Sharma",
+      img: "music-2",
+      src: "music-2"
+    },
+    {
+      name: "Ek Tara Kismat Da",
+      artist: "B Praak",
+      img: "music-3",
+      src: "music-3"
+    },
+    {
+      name: "Radhe Radhe Bol",
+      artist: "Hansraj Raghuvanshi",
+      img: "music-4",
+      src: "music-4"
+    },
+    {
+      name: "Aisi Khuda Dua De De",
+      artist: "Aisi Khuda Dua De De",
+      img: "music-5",
+      src: "music-5"
+    },
+    {
+      name: "Le Jaana Lekka",
+      artist: "Lekka",
+      img: "music-6",
+      src: "music-6"
+    },
+  ];
